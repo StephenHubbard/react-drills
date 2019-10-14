b9133f6b62573ec3ff100d9074339bf19d17d7c2
Stephen <hubbard_stephen@yaho.com>
2019-10-14 15:26:57 -0600

    up to api example

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor() {
+    super();
+
+    this.state={
+        family: ["Susie", "Brian", "Claire", "Dad", "Mom"]
+    }
   }
-}
+
+
+  render() {
+    let famDisplay = this.state.family.map((el, i) => {
+      return <h2 key={i}>{el}</h2>
+    })
+
+    return <div className="App">{famDisplay}</div>
+
+    };
+  }
+
 
 export default App;
